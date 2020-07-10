934f3b0a75bdd99e5b002499cc1e5867b7881a07
Rica <pudimdefrutas@gmail.com>
2020-07-10 20:56:07 -0300

    Improved players' movement + added collision

--- test/Player.gd
+++ test/Player.gd
@@ -1,10 +1,13 @@
-extends Node2D
+extends KinematicBody2D
 
-const SPEED = 200
+const SPEED = 4000
+const FRICTION_SPEED = 8000
+const MAX_SPEED = 400
+
+var movement = Vector2()
 
 func _ready():
 	pass # Replace with function body.
-
 
 func _process(delta):
 	var move_vec = Vector2()
@@ -16,5 +19,22 @@ func _process(delta):
 		move_vec += Vector2(-1,0)
 	if Input.is_action_pressed("player_right"):
 		move_vec += Vector2(1,0)
+	
 	move_vec = move_vec.normalized()
-	position += SPEED*move_vec*delta
+
+	if move_vec != Vector2.ZERO:
+		apply_movement(move_vec * SPEED * delta)
+	else:
+		apply_friction(FRICTION_SPEED * delta)
+	
+	movement = move_and_slide(movement)
+
+func apply_movement(acceleration):
+	movement += acceleration
+	movement = movement.clamped(MAX_SPEED)
+
+func apply_friction(acceleration):
+	if movement.length() > acceleration:
+		movement -= movement.normalized() * acceleration
+	else:
+		movement = Vector2()
