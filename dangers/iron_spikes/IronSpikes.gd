2712ad17e3629b10084476bc92a759913d6f49c4
Rica <pudimdefrutas@gmail.com>
2020-07-11 19:59:14 -0300

    Added spikes animation

--- dangers/iron_spikes/IronSpikes.gd
+++ dangers/iron_spikes/IronSpikes.gd
@@ -5,3 +5,11 @@ export var damage := 20
 func _on_Area2D_body_entered(body):
 	if body is Player:
 		body.take_damage(damage)
+	$Sprite.stop()
+	$Sprite.frame = 0
+	$Sprite.play()
+
+
+func _on_Sprite_animation_finished():
+	$Sprite.stop()
+	$Sprite.frame = 0
