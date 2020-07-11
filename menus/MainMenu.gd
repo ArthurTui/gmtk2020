8474f837d6606db075eba1c529287b7e8b8503ed
ArthurTui <arthur.vb@gmail.com>
2020-07-10 21:31:37 -0300

    Add transition between scenes

--- menus/MainMenu.gd
+++ menus/MainMenu.gd
@@ -2,8 +2,7 @@ extends Control
 
 
 func _on_NewGame_pressed():
-# warning-ignore:return_value_discarded
-	get_tree().change_scene("res://test/Test.tscn")
+	Transition.transition_to_scene("res://test/Test.tscn")
 
 
 func _on_Quit_pressed():
--- menus/Transition.gd
+++ menus/Transition.gd
@@ -1,21 +1,22 @@
 extends CanvasLayer
 
-signal fade_in_completed
-signal fade_out_completed
+signal completed
 
 onready var bg = $Background
 onready var tween = $Tween
 
-const DURATION := 1
+const DURATION := .5
 
 
-func transition_to_scene(path:String, duration := DURATION):
+func transition_to_scene(path:String, duration := DURATION, delay := 0):
 	tween.interpolate_property(bg, "color:a", 0, 1, duration)
+	tween.start()
 	yield(tween, "tween_completed")
-	emit_signal("fade_in_completed")
 	
+# warning-ignore:return_value_discarded
 	get_tree().change_scene(path)
 	
-	tween.interpolate_property(bg, "color:a", 1, 0, duration)
+	tween.interpolate_property(bg, "color:a", 1, 0, duration,
+			Tween.TRANS_LINEAR, Tween.EASE_IN, delay)
 	yield(tween, "tween_completed")
-	emit_signal("fade_out_completed")
+	emit_signal("completed")
