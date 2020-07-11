ca8976d5c852212748a7da45891cbaf8468f064e
Rica <pudimdefrutas@gmail.com>
2020-07-10 21:31:06 -0300

    Fixed collisions

--- test/Object.gd
+++ test/Object.gd
@@ -1,8 +1,13 @@
-extends Node2D
+extends StaticBody2D
+
+const BASE_WIDTH = 100
+const BASE_HEIGHT = 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Sprite.texture = get_sprite()
+	$Sprite.scale.x = float(BASE_WIDTH)/$Sprite.texture.get_width()
+	$Sprite.scale.y = float(BASE_HEIGHT)/$Sprite.texture.get_height()
 
 
 func get_sprite():
