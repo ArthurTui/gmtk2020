3ba283220d354d613d045c3edb81fe45588e2966
Rica <pudimdefrutas@gmail.com>
2020-07-12 06:38:28 -0300

    Added toxic fumes sfx

--- dangers/toxic_fumes/ToxicFumes.gd
+++ dangers/toxic_fumes/ToxicFumes.gd
@@ -6,6 +6,7 @@ func _on_Area2D_body_entered(body):
 	if body is Player:
 		body.take_damage(damage)
 		body.add_status(Status.TYPES.ACIDBURNING)
+		$Burst.play()
 		$Sprite.stop()
 		$Sprite.animation = "activating"
 		$Sprite.frame = 0
@@ -16,6 +17,7 @@ func _on_Area2D_body_entered(body):
 			$Sprite.animation = "active"
 			$Sprite.frame = 0
 			$Sprite.play()
+			$Loop.play()
 
 func _on_Area2D_body_exited(body):
 	if body is Player:
@@ -26,6 +28,7 @@ func _on_Area2D_body_exited(body):
 		$Sprite.play()
 		yield($Sprite, "animation_finished")
 		if $Sprite.animation == "deactivating":
+			$Loop.stop()
 			$Sprite.stop()
 			$Sprite.animation = "default"
 			$Sprite.frame = 0
