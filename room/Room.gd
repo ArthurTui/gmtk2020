af19ed6d3c951358c2503d3e4d6c694fbcfa3d09
ArthurTui <arthur.vb@gmail.com>
2020-07-12 02:10:58 -0300

    Add spawns to room

--- room/Room.gd
+++ room/Room.gd
@@ -1,6 +1,10 @@
 extends Node2D
 
 onready var bg = $Background
+onready var item_spawns = [$ItemSpawns/S1, $ItemSpawns/S2, $ItemSpawns/S3,
+		$ItemSpawns/S4]
+onready var opposite_spawns = [$ItemSpawns/S4, $ItemSpawns/S3, $ItemSpawns/S2,
+		$ItemSpawns/S1]
 
 
 func set_time_of_day(time:int):
