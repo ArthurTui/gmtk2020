d1d399cc68d32c070b585d08e8bbb83a97e1b391
Rica <pudimdefrutas@gmail.com>
2020-07-12 04:02:49 -0300

    Added first version of lava animation

--- dangers/lava/Lava.gd
+++ dangers/lava/Lava.gd
@@ -9,3 +9,13 @@ func _on_Area2D_body_entered(body):
 	if body is Player:
 		body.take_damage(damage)
 		$DamageSFX.play()
+		$Sprite.stop()
+		$Sprite.animation = "activate"
+		$Sprite.frame = 0
+		$Sprite.play()
+		yield($Sprite, "animation_finished")
+		$Sprite.stop()
+		$Sprite.animation = "default"
+		$Sprite.frame = 0
+		$Sprite.play()
+
