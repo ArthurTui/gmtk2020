ae34388f9be432e3e5ede676f5363567c150cad9
Rica <pudimdefrutas@gmail.com>
2020-07-12 09:14:26 -0300

    Added icons to safe items

--- items/SafeItem.gd
+++ items/SafeItem.gd
@@ -5,9 +5,9 @@ signal reached
 
 enum Types {BOOK, RING, BADGE, PILLS, MATCHES, PLANE}
 
-const images = [preload("res://icon.png"), preload("res://icon.png"),
-		preload("res://icon.png"), preload("res://icon.png"),
-		preload("res://icon.png"), preload("res://icon.png")]
+const images = [preload("res://assets/images/items/s6_chapeu formatura.png"), preload("res://assets/images/items/s2_buque.png"),
+		preload("res://assets/images/items/s1_carteira.png"), preload("res://assets/images/items/s5_vacina.png"),
+		preload("res://assets/images/items/s4_lanterna.png"), preload("res://assets/images/items/s3_carro.png")]
 
 var type : int
 
