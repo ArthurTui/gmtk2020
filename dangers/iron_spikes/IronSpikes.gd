956bd9aaf683378cb9a9687d9f9e2dbda31c3b7b
Rica <pudimdefrutas@gmail.com>
2020-07-12 05:15:49 -0300

    Added audio Desert Bus

--- dangers/iron_spikes/IronSpikes.gd
+++ dangers/iron_spikes/IronSpikes.gd
@@ -5,7 +5,7 @@ export var damage := 20
 func _on_Area2D_body_entered(body):
 	if body is Player:
 		body.take_damage(damage)
-		add_child(AudioManager.get_sfx("metalspike"))
+		$Damage.play()
 	$Sprite.stop()
 	$Sprite.frame = 0
 	$Sprite.play()
